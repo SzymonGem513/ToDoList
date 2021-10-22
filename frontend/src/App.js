166b57fecb2b4527ea623e46e227034a43d68b6e
szymongembalczyk <szymon.gembalczyk@ekookna.pl>
2021-10-22 12:54:36 +0200

    Add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Notes from './components/Notes/Notes';
+
+function App(){
+  return (
+    <div classname="App">
+      <Notes />
+    </div>
+  );
+};
+
+export default App;
